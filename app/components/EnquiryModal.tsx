088d5ce76935c4da91d17fec536aabeb539fd1c1
myadsparker <myadsparker@gmail.com>
2025-12-29 13:05:09 +0530

    aaa

--- app/components/EnquiryModal.tsx
+++ app/components/EnquiryModal.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
+import Image from "next/image";
 import CountryPhoneDropdown from "./CountryPhoneDropdown";
 
 interface EnquiryModalProps {
@@ -96,19 +97,7 @@ export default function EnquiryModal({ isOpen: controlledIsOpen, onClose: contro
                 rel="noopener noreferrer"
                 className="enquiry_icon_btn whatsapp_icon"
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z"></path>
-                </svg>
+                <Image src="/images/whatsapp.png" alt="WhatsApp" width={20} height={20} />
               </a>
             </div>
           </div>
